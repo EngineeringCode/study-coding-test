935ca63978cf97b7e4f2093b834a9d68c5c56c19
EngineeringCode <engineeringcode93@gmail.com>
2022-06-11 03:38:46 +0900

    홀수만 더하기 해답

---
+++ src/net/engineeringcode/codingtest/swexpertacademy/problem2072/Solution.java
@@ -0,0 +1,35 @@
+package net.engineeringcode.codingtest.swexpertacademy.problem2072;
+import java.util.*;
+
+class Solution
+{
+	/*
+	 * SW Expert Academy > CODE > Problem > 2072 > 홀수만 더하기
+	 * 문제주소: https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QSEhaA5sDFAUq
+	 * 작성자: 공학코드(engineeringcode93@gmail.com)
+	 * 
+	 */
+	
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            System.out.print("#"+test_case+" ");
+            int sum = 0;
+            for(int i=0; i<10; i++){
+                int currentValue = sc.nextInt();
+                if( (currentValue % 2) == 1){
+                 	sum = sum+currentValue;
+                }
+                if(i==9){
+                    System.out.print(sum);   
+                }
+            }
+            System.out.println();
+		}
+	}
+}
